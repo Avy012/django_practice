c0cb5a34dab96f17a5610ba28b7f666b806af7c6
Avy012 <kist0907@gmail.com>
2024-09-15 21:03:15 +0900

    blog post list page with CBV

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,31 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import ListView
+
 from .models import Post
 
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
 
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts':posts,
-        }
-    )
-def single_post_pages(request,pk):
-    post=Post.objects.get(pk=pk)
-
-    return render(
-        request,
-        'blog/single_post_page.html',
-        {
-            'post':post,
-        }
-    )
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts':posts,
+#         }
+#     )
+# def single_post_pages(request,pk):
+#     post=Post.objects.get(pk=pk)
+#
+#     return render(
+#         request,
+#         'blog/single_post_page.html',
+#         {
+#             'post':post,
+#         }
+#     )
