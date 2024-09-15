6fe4d7f73ecfc560601d171b4789382353dcdd54
Avy012 <kist0907@gmail.com>
2024-09-15 20:07:52 +0900

    single post pages with FBV

--- blog/views.py
+++ blog/views.py
@@ -14,3 +14,13 @@ def index(request):
             'posts':posts,
         }
     )
+def single_post_pages(request,pk):
+    post=Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post':post,
+        }
+    )
